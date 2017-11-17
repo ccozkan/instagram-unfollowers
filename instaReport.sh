fd4615eddc635fd974c2f40bea457818098b13c8
Çağrı Özkan <ozkancc@yandex.com>
2017-11-17 21:45:00 +0300

    Create instaReport.sh

---
+++ instaReport.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+/usr/bin/python /home/pi/instagram-unfollowers/main.py 528817151 
+
+/bin/echo "brtyb github.com/cagrio" | /usr/bin/mutt -a "output.html" -s "instagram weekly follow report" -- username@emailadress.com 
